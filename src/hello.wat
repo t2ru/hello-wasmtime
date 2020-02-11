3c20c33161c7238f0276faf53de2324c85a17faf
Tetsuya Takatsuru <tetsuya@taka2ru.jp>
2020-02-11 17:31:10 +0900

    calling back to rust

--- src/hello.wat
+++ src/hello.wat
@@ -1,8 +1,17 @@
 (module
+    (func $print_number (import "" "print_number") (param i32))
+
     (func (export "add") (param $lhs i32) (param $rhs i32) (result i32)
         local.get $lhs
         local.get $rhs
         i32.add
         return
+    )
+
+    (func (export "add_print") (param $lhs i32) (param $rhs i32)
+        local.get $lhs
+        local.get $rhs
+        i32.add
+        call $print_number
     )
 )
--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,23 @@ fn main() {
     let hello_wasm = wat::parse_bytes(hello_wat.as_ref()).expect("wat parse error");
 
     let store = wasmtime::Store::default();
-    let module = wasmtime::Module::new(&store, &hello_wasm).unwrap();
-    let instance = wasmtime::Instance::new(&module, &[]).expect("wasm instance");
+    let module = wasmtime::Module::new(&store, &hello_wasm).expect("make module");
+
+    let exports = [wasmtime::Extern::Func(wasmtime::Func::wrap1(&store, print_number))];
+    let instance = wasmtime::Instance::new(&module, &exports).expect("wasm instance");
 
     let add = instance
         .get_export("add")
         .and_then(|sym| sym.func())
         .unwrap();
-    let result = add.call(&[Val::I32(2), Val::I32(3)]).unwrap();
+    let result = add.call(&[Val::I32(2), Val::I32(3)]).expect("call add");
     println!("Result: {:?}", result);
+
+    let add_print = instance.get_export("add_print").and_then(|sym| sym.func()).unwrap();
+    let result = add_print.call(&[Val::I32(2), Val::I32(3)]).expect("call add_print");
+    println!("Result: {:?}", result);
+}
+
+fn print_number(n: i32) {
+    println!("numner = {}", n);
 }
